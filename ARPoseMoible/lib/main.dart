d33dce7e980c1efabd82657a08f4a50ab21f790f
jorisCPE <113911658+jorisCPE@users.noreply.github.com>
2025-11-19 11:48:18 +0900

    suppression de la home page acces direct a la webview au lancement de l'application

--- ARPoseMoible/lib/main.dart
+++ ARPoseMoible/lib/main.dart
@@ -28,39 +28,7 @@ class ARPoseApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const HomePage(),
-    );
-  }
-}
-
-// Page d'accueil avec le bouton
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Page d\'accueil'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const WebViewPage(),
-              ),
-            );
-          },
-          style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-            textStyle: const TextStyle(fontSize: 18),
-          ),
-          child: const Text('Ouvrir WebView'),
-        ),
-      ),
+      home: const WebViewPage(),
     );
   }
 }
